542f90396e9cdbf84a7c9d61a64525ab80e78da6
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-08-22 04:16:57 +0300

    Try new shader

--- gol.glsl
+++ gol.glsl
@@ -1,78 +1,31 @@
 #[compute]
-
 #version 450
+// Taken and modifier from https://github.com/yumcyaWiz/glsl-compute-shader-sandbox/blob/main/sandbox/life-game/shaders/update-cells.comp
+layout(local_size_x = 8, local_size_y = 8) in;
 
-// Ported from https://github.com/WorldOfZero/UnityVisualizations/blob/master/GameOfLife/ConwaysGameOfLifeSimulation.compute
+layout(set = 0, binding = 0, rgba32f) uniform image2D cells_in;
+// layout(r8ui, binding = 1) uniform uimage2D cells_out;
 
-layout(local_size_x = 8, local_size_y = 1, local_size_z = 1) in;
-layout(set = 0, binding = 0, rgba32f) uniform image2D target_image;
+uint updateCell(ivec2 cell_idx) {
+  float current_status = imageLoad(cells_in, cell_idx).x;
 
-//layout(set = 0, binding = 0, rgba32f) writeonly uniform image2D color_buffer;
+  float alive_cells = 0.0;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(-1, -1)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(-1, 0)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(-1, 1)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(0, -1)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(0, 1)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(1, -1)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(1, 0)).x;
+  alive_cells += imageLoad(cells_in, cell_idx + ivec2(1, 1)).x;
+
+  return uint(current_status == 0.0 && alive_cells == 3.0) + uint(current_status == 1.0 && alive_cells > 1.0 && alive_cells < 4.0);
+}
 
 void main() {
-	ivec2 index = ivec2(gl_GlobalInvocationID.xy);
-// 	ivec2 size = imageSize(target_image);
-	//vec2 position = (vec2(index) + 0.5) / vec2(size);
-// 	vec2 pixelSize = vec2(1.0 / size.x, 1.0 / size.y);
+  ivec2 gidx = ivec2(gl_GlobalInvocationID.xy);
 
-// 	vec4 currentPixel = imageLoad(target_image, ivec2(position.x, position.y));
-	vec4 currentPixel = imageLoad(target_image, index);
+  uint next_status = updateCell(gidx);
 
-	vec4 neighborPixels = vec4(0, 0, 0, 0);
-	// +Y
-	neighborPixels += imageLoad(target_image, index + ivec2(1, 1));
-	neighborPixels += imageLoad(target_image, index + ivec2(0, 1));
-	neighborPixels += imageLoad(target_image, index + ivec2(-1, 1));
-	// Neutral Y
-	neighborPixels += imageLoad(target_image, index + ivec2(1, 0));
-	neighborPixels += imageLoad(target_image, index + ivec2(-1, 0));
-	// -Y
-	neighborPixels += imageLoad(target_image, index + ivec2(1, -1));
-	neighborPixels += imageLoad(target_image, index + ivec2(0, -1));
-	neighborPixels += imageLoad(target_image, index + ivec2(-1, -1));
-
-	// +Y
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x + pixelSize.x, position.y + pixelSize.y));
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x, position.y + pixelSize.y));
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x - pixelSize.x, position.y + pixelSize.y));
-// 	// Neutral Y
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x + pixelSize.x, position.y));
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x - pixelSize.x, position.y));
-// 	// -Y
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x + pixelSize.x, position.y - pixelSize.y));
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x, position.y - pixelSize.y));
-// 	neighborPixels += imageLoad(target_image, ivec2(position.x - pixelSize.x, position.y - pixelSize.y));
-
-	// Add current pixel for reason
-	//neighborPixels += currentPixel;
-
- 	if (currentPixel.r > 0.5) {
- 		if (neighborPixels.r > 1.5 && neighborPixels.r < 3.5) { // Between 2 and 3
- 			imageStore(target_image, index, vec4(1, 1, 1, 1));
- 		}
- 		else {
- 			imageStore(target_image, index, vec4(0, 0, 0, 1));
- 		}
- 	}
- 	else {
- 		if (neighborPixels.r > 2.5 && neighborPixels.r < 3.5) { // == 3
- 			imageStore(target_image, index, vec4(1, 1, 1, 1));
- 		}
- 		else {
- 			imageStore(target_image, index, vec4(0, 0, 0, 1));
- 		}
- 	}
-// 	imageStore(target_image, index, vec4(0, 1, 0, 0.5));
-// 	imageStore(target_image, index, imageLoad(target_image, index - ivec2(1)));
-// 	imageStore(target_image, index, currentPixel);
-// 	imageStore(target_image, index, neighborPixels / 9.0);
-
-// 	if (index.x >= size.x || index.y >= size.y) {
-// 		return;
-// 	}
-//
-// 	vec2 uv = (vec2(index) + 0.5) / vec2(size);
-// 	imageStore(target_image, index, vec4(uv, 0, 1.0));
-	//uint gid = gl_GlobalInvocationID.x;
-	//color_buffer.data[gid] = gid;
+  imageStore(cells_in, gidx, uvec4(next_status));
 }
